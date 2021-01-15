0f898ab86c986bbfb346e4df5f7d29820d69dfd7
Jeongjun <hsk2454@naver.com>
2021-01-15 17:46:31 +0900

    과제중

---
+++ baekjoon/13주차/동굴탐험/정준/CaveExplore.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.Stack;
+
+import jdk.internal.org.jline.utils.InputStreamReader;
+
+public class CaveExplore {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    }
+
+    public boolean solution(int n, int[][] path, int[][] order) {
+        boolean answer = true;
+        
+        Stack<Integer> s = new Stack<>();
+        
+
+        return answer;
+    }
+}
